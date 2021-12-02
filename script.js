5899e9c1be108740f447cbbc93123138936b7335
pollyleyka <polka321@rambler.ru>
2021-12-02 23:47:21 +0300

    валидация пароля!
    заработало

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 let form = document.querySelector('.form');
 let password = document.getElementById('password-input');
 let pwdConfirm = document.getElementById('password-input-valid');
-let pwdValue = password.value;
-const hasUpperCase = /[A-Z]/.test(pwdValue);
-const hasLowerCase = /[a-z]/.test(pwdValue);
-const hasDigital = /\d/.test(pwdValue);
-
+form.addEventListener ('submit', function(event) {
+event.preventDefault();
+const upRegister = /[A-Z]/.test(password.value);
+const hasLowerCase = /[a-z]/.test(password.value);
+const hasDigital = /\d/.test(password.value);
 const checkPassword = () => {
   if (password.value !== pwdConfirm.value) {
     return false;
   }
-  else if (pwdValue.length < 8) {
+  else if (password.value.length < 8) {
     return false;
  }
   else
- if (hasUpperCase && hasDigital && hasLowerCase) {
+ if (upRegister && hasDigital && hasLowerCase) {
     return true;
   }
 };
-
-form.addEventListener ('submit', function(event) {
-event.preventDefault();
-const valid = checkPassword()
+const valid = checkPassword();
 if (valid) {
   form.submit();
 }
 else console.log('no');
 });
+
+// let form = document.querySelector('.form');
+// let password = document.getElementById('password-input');
+//
+// form.addEventListener ('submit', function(event) {
+// event.preventDefault();
+// console.log(/[A-Z]/.test(password.value));});
