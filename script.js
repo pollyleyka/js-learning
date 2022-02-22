fb6a2301e042c4dad7356590f21a5da50639c89a
pollyleyka <polka321@rambler.ru>
2022-02-22 22:49:10 +0400

    попытка оптимизировать+ изменение img через src
    оптимизировать не получилось т.к. .value работает только если объявить внутри функции.
    
    как менять src поняла, не успела доделать глазик.
    
    Итого валидация работает, глазик в процессе

--- script.js
+++ script.js
@@ -1,69 +1,75 @@
-let form = document.querySelector('.form');
-let password = document.getElementById('password-input');
-let pwdConfirm = document.getElementById('password-input-valid');
+ const form = document.querySelector('.form');
+ const password = document.getElementById('password-input');
+ const pwdConfirm = document.getElementById('password-input-valid');
 
-//подсвечиваем красным поле ввода если не выполняются условия пароля:
-password.addEventListener('change', (event) => {
-  const pwdInput = password.value
-  const upRegister = /[A-Z]/.test(pwdInput);
-  const hasLowerCase = /[a-z]/.test(pwdInput);
-  const hasDigital = /\d/.test(pwdInput);
+ //попытка оптимизировать не удалась:
+ //const pwdInput = password.value; //что не так с этой переменной!??? не работает если объявлять за пределами функции. внутри функции все ок.
+ //let pwdInput = password.value; так тоже не работает. Видимо работает только если объявлять внутри функции т.к. данные динамические.
+ //let upRegister = /[A-Z]/.test(password.value); - зе сейм тк тут тоже вэлью.
+ //const hasLowerCase = /[a-z]/.test(password.value);
+ //const hasDigital = /\d/.test(password.value);
+ // таким образом оптимизировать нельзя, не будут находиться значения т.к. не работает вэлью. Либо я не знаю как можно это обойти.
 
-  if (pwdInput.length < 8 || !upRegister || !hasDigital || !hasLowerCase) {
-  event.target.classList.add('invalid');
-} else {
-  event.target.classList.remove('invalid');
-}
-});
+ 
+ //подсвечиваем красным поле ввода если не выполняются условия пароля:
+ password.addEventListener('input', (event) => {
+   const upRegister = /[A-Z]/.test(password.value);
+   const hasLowerCase = /[a-z]/.test(password.value);
+   const hasDigital = /\d/.test(password.value);
+   if (password.value.length < 8 || !upRegister || !hasDigital || !hasLowerCase) {
+     event.target.classList.add('invalid');
+   } else {
+     event.target.classList.remove('invalid');
+   }
+ });
 
-form.addEventListener ('submit', function(event) {
-event.preventDefault();
-
-const upRegister = /[A-Z]/.test(password.value);
-const hasLowerCase = /[a-z]/.test(password.value);
-const hasDigital = /\d/.test(password.value);
-
-        const checkPassword = () => {
-          if (password.value !== pwdConfirm.value) {
-            return false;
-          }
-          else if (password.value.length < 8) {
-            return false;
-         }
-          else if (upRegister && hasDigital && hasLowerCase) {
-            return true;
-          }
-        };
- const valid = checkPassword();
-    if (valid) {
-      form.submit();
-    }
-    else console.log('no');
-    });
+ // верификация при нажатии кнопки отправить
+ form.addEventListener('submit', (evt) => {
+   evt.preventDefault();
+   let upRegister = /[A-Z]/.test(password.value);
+   let hasLowerCase = /[a-z]/.test(password.value);
+   let hasDigital = /\d/.test(password.value);
+   if (password.value.length >= 8 &&
+     password.value === pwdConfirm.value &&
+     upRegister && hasDigital && hasLowerCase) {
+     form.submit();
+   } else {
+     console.log("no");
+   }
+ });
 
 
-//отображение пароля по клику на глаз
+ //отображение пароля по клику на глаз
 
-const eyeBtn = document.querySelectorAll(".eye");
-const eyeSlashBtn = document.querySelectorAll(".eye-slash");
+ eyeBtn = document.querySelector(".eye");
+ eyeBtn.addEventListener('click', (event) => {
+   const btn = event.target;
+   eyeBtn.setAttribute("src", "pic/eye.png");
+   btn.parentElement.querySelector('.password').type = 'text';
+ });
 
-eyeSlashBtn.forEach((b) => {
-    b.addEventListener('click', (event) => {
-        const btn = event.target;
-        btn.classList.add('hidden');
-        btn.parentElement.querySelector('.eye').classList.remove('hidden');
-        btn.parentElement.querySelector('.password').type = 'text';
+ //доделаю
 
-    });
-})
+ //  const eyeBtn = document.querySelectorAll(".eye");
+ //  const eyeSlashBtn = document.querySelectorAll(".eye-slash");
 
-eyeBtn.forEach((c) => {
-  c.addEventListener('click', (event) => {
-    const btn = event.target;
-    btn.classList.add('hidden');
-    btn.parentElement.querySelector('.eye-slash').classList.remove('hidden');
-    btn.parentElement.querySelector('.password').type = 'password';
+ //  eyeSlashBtn.forEach((b) => {
+ //    b.addEventListener('click', (event) => {
+ //      const btn = event.target;
+ //      btn.classList.add('hidden');
+ //      btn.parentElement.querySelector('.eye').classList.remove('hidden');
+ //      btn.parentElement.querySelector('.password').type = 'text';
 
-   });
+ //    });
+ //  });
 
-})
+ //  eyeBtn.forEach((c) => {
+ //    c.addEventListener('click', (event) => {
+ //      const btn = event.target;
+ //      btn.classList.add('hidden');
+ //      btn.parentElement.querySelector('.eye-slash').classList.remove('hidden');
+ //      btn.parentElement.querySelector('.password').type = 'password';
+
+ //    });
+
+ //  });
